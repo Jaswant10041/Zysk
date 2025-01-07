4585a2ab6302065d0c4654f51f56e919d12cd2b6
Jaswant10041 <jaswanthkumarmadineni@gmail.com>
2025-01-08 00:36:58 +0530

    msg

--- src/Second.js
+++ src/Second.js
@@ -5,7 +5,7 @@ const Second = () => {
     <div className="flex flex-col items-center">
      
       
-      <img  className= "flex justify-center py-10" src="https://i.im.ge/2025/01/07/zJQDrm.Screenshot-2025-01-07-233655.png" width={1000} />
+      <img  className= "flex justify-center py-10" src="https://i.ibb.co/9Tc6fWV/cloudinay.png" width={1000} />
      
     </div>
 
